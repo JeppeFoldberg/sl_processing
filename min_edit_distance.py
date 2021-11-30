86acee26c8c2474a1a73b1e0aaaf7cd4bd267eba
JeppeFoldberg <jeppe_05@msn.com>
2021-11-30 08:34:33 +0100

    fixed min_edit_distance

--- min_edit_distance.py
+++ min_edit_distance.py
@@ -22,35 +22,31 @@ def min_edit_distance(source: str, target: str) -> int:
     # distance matrix
     D = numpy.zeros((n + 1, m + 1))
 
-    for i in range(1, n):
+    for i in range(1, n + 1):
         # deletion cost
         D[i, 0] = D[i - 1, 0] + 1
-    for j in range(1, m):
+    for j in range(1, m + 1):
         # insertion cost
         D[0, j] = D[0, j-1] + 1
 
     print(D)
 
     # recurrence relation
-    for i in range(1, n):
-        for j in range(1, m):
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
 
             D[i, j] = min(
                 D[i-1, j] + 1,
-                D[i-1, j-1] + sub_cost(source[i], target[j]),
+                D[i-1, j-1] + sub_cost(source[i - 1], target[j - 1]),
                 D[i, j-1] + 1,
             )
-
-    print(D)
 
     return D[n, m]
 
 
-    
-
 
 def main():
-    distance = min_edit_distance("yeah", "yeahbo")
+    distance = min_edit_distance("intention", "execution")
     print(distance)
 
 if __name__ == "__main__":
